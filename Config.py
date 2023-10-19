da5dca3a5ce0f59134b02068bfe2619d5165a61f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-20 00:40:06 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,11 +6,11 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 LOGGER = loguru.logger
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 13675555))
+        API_ID = int(os.environ.get('API_ID', '13675555'))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', 'c0da9c346d2c45dbc7ec49a05da9b2b6')
-    OWNER_ID = os.environ.get('OWNER_ID', 5591954930)
+    OWNER_ID = os.environ.get('OWNER_ID', '5591954930')
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc')
     DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://apfieuka:ADzi89DykPp6i99R-GvnH_1nxsWjVLSQ@stampy.db.elephantsql.com/apfieuka')
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
